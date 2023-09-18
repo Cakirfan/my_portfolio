fe274a0d2532cf7cbcb49854d502093d57b8450d
irfan <cakirfan.001@gmail.com>
2023-09-18 11:51:52 +0200

    portfolio updated

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,33 +4,39 @@ import { portWorks } from "../../helper/data"
 
 const Portfolio = ()=> {
     return (
-        <section id="portfolio">
+      <section id="portfolio">
+        <h1 className="sub-heading">Portfolio</h1>
+        <div className="divider"></div>
+        <p className="sub-paraf">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis quo
+          similique qui distinctio quae facere harum doloribus mollitia sed
+          nihil?
+        </p>
 
-            <h1 className="sub-heading">Portfolio</h1>
-            <div className="divider"></div>
-            <p className="sub-paraf">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis quo similique qui distinctio quae facere harum doloribus mollitia sed nihil?
-            </p>
-
-            <div className="port-row">
-                {
-                    portWorks.map((work) => (
-                    <div className="port-item" key={work.wId}>
-                        <div className="port-img">
-                            <img src={work.wImage} alt={work.wName} />
-                        </div>
-                        <div className="port-info">
-                            <h4>{work.wName}</h4>
-                            <p>{work.wDesc}</p>
-                            <a href="#"><FaUpRightFromSquare /></a>
-                        </div>
-                    </div>                    
-                    ))
-                }
+        <div className="btn-group">
+          <button className="active">all</button>
+          <button>easy</button>
+          <button>intermediate level</button>
+          <button>high level</button>
+        </div>
+        <div className="port-row">
+          {portWorks.map((work) => (
+            <div className="port-item" key={work.wId}>
+              <div className="port-img">
+                <img src={work.wImage} alt={work.wName} />
+              </div>
+              <div className="port-info">
+                <h4>{work.wName}</h4>
+                <p>{work.wDesc}</p>
+                <a href="#">
+                  <FaUpRightFromSquare />
+                </a>
+              </div>
             </div>
-
-        </section>
-    )
+          ))}
+        </div>
+      </section>
+    );
 }
 
 export default Portfolio;
